b7de96a0b14cb69f46b663988d67f89bf7b89767
riperjew <roman.amur@gmail.com>
2021-12-18 20:48:05 +0200

    x level fix

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -876,7 +876,7 @@
   // Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
   // If not defined, probe limits will be used.
   // Override with 'M422 S<index> X<pos> Y<pos>'
-  #define Z_STEPPER_ALIGN_XY { {  30, 105 }, { 105,  10 }, { 190, 190 } }
+  #define Z_STEPPER_ALIGN_XY { {  30, 105 }, { 210,  105 },  }
 
   /**
    * Orientation for the automatically-calculated probe positions.
@@ -908,7 +908,7 @@
     // Define Stepper XY positions for Z1, Z2, Z3 corresponding to
     // the Z screw positions in the bed carriage.
     // Define one position per Z stepper in stepper driver order.
-    #define Z_STEPPER_ALIGN_STEPPER_XY { { 210.7, 102.5 }, { 152.6, 220.0 }, { 94.5, 102.5 } }
+    //#define Z_STEPPER_ALIGN_STEPPER_XY { { 210.7, 102.5 }, { 152.6, 220.0 }, { 94.5, 102.5 } }
   #else
     // Amplification factor. Used to scale the correction step up or down in case
     // the stepper (spindle) position is farther out than the test point.
